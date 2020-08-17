b0b8949826e21b87ced632174f020eeb57a71bfe
Nikko B <ncbadoles@gmail.com>
2020-08-17 23:38:04 +0000

    examples of join queries

---
+++ 2_queries_joins/1_total_as_dur.sql
@@ -0,0 +1,10 @@
+/*
+This should work for any student but use 'Ibrahim Schimmel' for now.
+Select only the total amount of time as a single column. 
+*/
+
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
+
